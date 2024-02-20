70c0ec0a65a5fcc329e540d3d9a1b838be55db53
Andro Chilachava <157217297+AndroGenium@users.noreply.github.com>
2024-02-20 20:42:55 +0400

    Add files via upload

---
+++ Variables/script.js
@@ -0,0 +1,56 @@
+//1. char
+
+let char = "@"
+
+//2. string
+
+let string = "zd"
+
+//3. number
+
+const pi = 3.14
+
+//4. boolean
+
+let bool = true
+
+//5. null
+
+const zero = null
+
+//6. array
+
+let array = [1, 2, 3, 4, 5]
+
+//7. object
+
+const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"}
+
+
+console.log(typeof char)
+
+console.log(char)
+
+console.log("...")
+
+console.log(typeof string)
+console.log(string)
+console.log("...")
+
+console.log(typeof pi)
+console.log(pi)
+
+console.log("...")
+
+console.log(typeof bool)
+console.log( bool)
+console.log("...")
+console.log(typeof zero)
+console.log(zero)
+console.log("...")
+console.log(typeof array)
+console.log(array)
+console.log("...")
+console.log(typeof person)
+console.log( person)
+console.log("...")
